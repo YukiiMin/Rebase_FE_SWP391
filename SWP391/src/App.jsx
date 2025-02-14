24d411ec021848dfea58938a78eeef715a731758
CuongNM28123 <ngmanhcuong28123@gmail.com>
2025-02-14 16:34:37 +0700

    Basic Routing

--- SWP391/src/App.jsx
+++ SWP391/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Navigation from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPAge";
+import PriceListPage from "./pages/PriceListPage";
 
 function App() {
 	return (
 		<div>
 			<Navigation />
+			<Routes>
+				<Route path={"/"} element={<HomePage />} />
+				<Route path={"/AboutUs"} element={<AboutUsPage />} />
+				<Route path={"/PriceList"} element={<PriceListPage />} />
+			</Routes>
 		</div>
 	);
 }
--- SWP391/src/components/Navbar.jsx
+++ SWP391/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Navigation() {
 	return (
 		<Navbar bg="dark" data-bs-theme="dark">
 			<Container>
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+				<Navbar.Brand href="/">Navbar</Navbar.Brand>
 				<Nav className="me-auto">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#features">Features</Nav.Link>
-					<Nav.Link href="#pricing">Pricing</Nav.Link>
+					<NavLink to={"/"} className={"nav-link"}>
+						Home
+					</NavLink>
+					<NavLink to={"/AboutUs"} className={"nav-link"}>
+						About Us
+					</NavLink>
+					<NavLink to={"/PriceList"} className={"nav-link"}>
+						Price List
+					</NavLink>
 				</Nav>
 				<Nav className="me-auto">
 					<Nav.Link>Login</Nav.Link>
